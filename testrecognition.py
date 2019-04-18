f12d7db664ea1fd72af14ae2a92c8175e7dbfaab
Polina <chukhninap@gmail.com>
2019-04-18 15:41:35 +0300

    add function pca

--- testrecognition.py
+++ testrecognition.py
@@ -19,6 +19,19 @@ def image_to_array(filename):
     return data
 
 
+# create face matrix
+@jit(float32(int32))
+def face_matrix(count):
+
+    face_matr = np.zeros((count, STANDART_SHAPE))
+    for i in range(count):
+        face = image_to_array("image" + i + ".jpg")
+        for j in range(STANDART_SHAPE):
+            face_matr[i][j] = face[j]
+
+    return face_matr
+
+
 # the average face - normalized face matrix
 @jit(float32(int32, float32))
 def average_face(av_face, face_matrix):
@@ -36,3 +49,22 @@ def average_face(av_face, face_matrix):
     return face_matrix
 
 
+# calculate pca
+@jit(float32(int32, float32))
+def pca(temp_transp, face_matr):
+
+    for i in range(STANDART_SHAPE):
+        for j in range(face_matr.shape[0]):
+            temp_transp[i][j] = face_matr[j][i]
+
+    cov_matr = np.matmul(face_matr, temp_transp)
+
+    return cov_matr
+
+
+temp_faces = face_matrix(4)
+temp_average = np.zeros(STANDART_SHAPE)
+faces = average_face(temp_average, temp_faces)
+temp_tr = np.zeros((STANDART_SHAPE, 4))
+temp_cov = pca(temp_tr, faces)
+print(temp_cov.shape)
