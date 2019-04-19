4e5bb0a8d5b0cd8d76a1f951c89e9cee2a608f31
Polina <chukhninap@gmail.com>
2019-04-19 22:39:28 +0300

    add class and some function

--- testrecognition.py
+++ testrecognition.py
@@ -20,12 +20,12 @@ def image_to_array(filename):
 
 
 # create face matrix
-@jit(float32(int32))
-def face_matrix(count):
+@jit()
+def face_matrix(count, filename):
 
     face_matr = np.zeros((STANDART_SHAPE, count))
     for i in range(count):
-        face = image_to_array("image" + str(i) + ".jpg")
+        face = image_to_array(filename + str(i) + ".jpg")
         for j in range(STANDART_SHAPE):
             face_matr[j][i] = face[0][j]
 
@@ -50,21 +50,67 @@ def average_face(av_face, face_matr):
 
 
 # calculate pca
-@jit(float32(int32, float32))
-def pca(temp_transp, face_matr):
+def pca(face_matr):
 
+    temp_transp = np.zeros((face_matr.shape[1], STANDART_SHAPE))
     for i in range(face_matr.shape[1]):
         for j in range(STANDART_SHAPE):
             temp_transp[i][j] = face_matr[j][i]
 
     cov_matr = np.matmul(temp_transp, face_matr)
+    eigvals, eigvecs = np.linalg.eig(cov_matr)
 
-    return cov_matr
+    shape = eigvecs.shape[0]
+    new_eigvecs = np.zeros((shape, STANDART_SHAPE))
+
+    for i in range(shape):
+        for j in range(shape):
+            for k in range(STANDART_SHAPE):
+                new_eigvecs[i][k] += eigvecs[i][j] * face_matr[k][j]
+
+    return new_eigvecs
+
+# projected faces into face space - матрица лиц уже нормирована по среднему лицу
+@jit()
+def transform_new_face(face, face_matr, eigenvecs):
+
+    count = eigenvecs.shape[0]
+    weight_vec = np.zeros(count)
+    for i in range(count):
+        temp = eigenvecs[i, :]
+        weight_vec[i] = np.matmul(temp, face)
+
+    return weight_vec
 
 
-test_faces = face_matrix(4)
-test_avface = np.zeros(STANDART_SHAPE)
-change_faces = average_face(test_avface, test_faces)
-test_transp = np.zeros((4, STANDART_SHAPE))
-test_cov = pca(test_transp, change_faces)
-print(test_cov.shape)
+class CalculatePCA(object):
+
+    def __init__(self, count_train, filename_train, filename_test):
+        self.count_train_set = count_train
+        self.count_test_set = 1
+        self.av_face = np.zeros(STANDART_SHAPE)
+        self.face_matr_train = face_matrix(self.count_train_set, filename_train)
+        self.face_matr_test = face_matrix(self.count_test_set, filename_test)
+        self.eigvecs = np.zeros((self.count_train_set, STANDART_SHAPE))
+
+    def calc_pca(self):
+        self.face_matr_train = average_face(self.av_face, self.face_matr_train)
+        self.eigvecs = pca(self.face_matr_train)
+
+        return self.eigvecs
+
+    def transform_faces(self):
+        self.face_matr_test = average_face(self.av_face, self.face_matr_test)
+
+        return transform_new_face(self.face_matr_test, self.face_matr_train, self.eigvecs)
+
+
+test = CalculatePCA(3, "image", "image0")
+test.calc_pca()
+temp_test = test.transform_faces()
+print(temp_test)
+
+
+
+
+
