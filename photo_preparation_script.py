5b9d2ef603a0419dcf0a000d90ac791ac7a92b7b
Polina <chukhninap@gmail.com>
2019-09-28 13:14:38 +0300

    change all

---
+++ photo_preparation_script.py
@@ -0,0 +1,9 @@
+import os
+
+os.chdir("/home/polina/Рабочий стол/facedb3/faces94")
+
+female = os.listdir("female")
+male = os.listdir("male")
+malestaff = os.listdir("malestaff")
+
+
--- temp.py
+++ temp.py
@@ -1,29 +1,91 @@
 import testrecognition as fr
+import numpy as np
+import matplotlib.pyplot as plt
 
-test = fr.CalculatePCA(5, 'train')
+# for plot
+x = np.arange(40, 62, 2)
+y = np.zeros(x.shape[0])
+t = x[1] - x[0]
+
+test = fr.CalculatePCA(x[0], 'train')
 test.calc_pca()
-print('PCA - 5 photos')
+print('PCA - ' + str(x[0]) + ' photos')
 count_true = 0
-for i in range(5):
-    print('True answer: train' + str(i) + '.pgm')
-    print('Test image: test' + str(2 * i) + '0.pgm')
-    r = test.transform_faces('test' + str(2 * i))
-    if r == i:
-        count_true += 1
-    print('Test image: test' + str(2 * i + 1) + '0.pgm')
-    r = test.transform_faces('test' + str(2 * i + 1))
-    if r == i:
-        count_true += 1
-    print('\n')
-print('Count true:' + str(count_true) + ' / Count tests:' + str(2 * 5))
-print('Quality = ' + str(count_true / 10 * 100) + '%')
+count_test = 3
+# for i in range(x[0]):
+#     for j in range(count_test):
+#         r = test.transform_faces('test' + str(count_test * i + j))
+#         if r == i:
+#             count_true += 1
+# print('Count true:' + str(count_true) + ' / Count tests:' + str(count_test * x[0]))
+# print('Quality = ' + str((count_true / (count_test * x[0]) * 100)) + '% ')
+# print("-------------------------------------------------------------------------\n")
+# y[0] = (count_true / (x[0] * count_test)) * 100
+#
+# for k in range(1, x.shape[0]):
+#     test.pca_without_train(x[k], 'train')
+#     print('PCA without new train - ' + str(x[k]) + ' photos')
+#     count_true = 0
+#     for i in range(x[k]):
+#         for j in range(count_test):
+#             r = test.transform_faces('test' + str(count_test * i + j))
+#             temp_r = divmod(r, 40)[1]
+#             if r == i:
+#                 count_true += 1
+#     print('Count true:' + str(count_true) + ' / Count tests:' + str(count_test * x[k]))
+#     print('Quality = ' + str((count_true / (count_test * x[k])) * 100) + '%')
+#     print("--------------------------------------------------------------------------\n")
+#     y[k] = (count_true / (count_test * x[k])) * 100
+#
+# graphic = plt.plot(x, y)
+# plt.grid(True)
+# plt.scatter(x, y)
+# plt.xlabel('Количество фотографий. Шаг - ' + str(t) + ' фото')
+# plt.ylabel('Точность распознавания')
+# plt.show(graphic)
 
-test.pca_without_train(10, 'train')
-print('PCA without new train - 10 photos')
-for i in range(10):
-    print('True answer: train' + str(i) + '.pgm')
-    print('Test image: test' + str(2 * i) + '0.pgm')
-    test.transform_faces('test' + str(2 * i))
-    print('Test image: test' + str(2 * i + 1) + '0.pgm')
-    test.transform_faces('test' + str(2 * i + 1))
-    print('\n')
+
+# x = np.arange(40, 71, 1)
+# y = np.zeros(x.shape[0])
+# t = x[1] - x[0]
+#
+# test = fr.CalculatePCA(x[0], 'train')
+# test.calc_pca()
+# print('PCA - ' + str(x[0]) + ' photos')
+# count_true = 0
+# count_test = 3
+# for i in range(x[0]):
+#     for j in range(count_test):
+#         r = test.transform_faces('test' + str(count_test * i + j))
+#         if r == i:
+#             count_true += 1
+#     # print('\n')
+# print('Count true:' + str(count_true) + ' / Count tests:' + str(count_test * x[0]))
+# print('Quality = ' + str((count_true / (count_test * x[0]) * 100)) + '% ')
+# print("-------------------------------------------------------------------------\n")
+# y[0] = (count_true / (x[0] * count_test)) * 100
+#
+# for k in range(1, x.shape[0]):
+#     test.pca_without_train(x[k], 'train')
+#     print('PCA without new train - ' + str(x[k]) + ' photos')
+#     count_true = 0
+#     for i in range(x[k]):
+#         for j in range(count_test):
+#             r = test.transform_faces('test' + str(count_test * i + j))
+#             temp_r = divmod(r, 40)[1]
+#             if r == i:
+#                 count_true += 1
+#     print('Count true:' + str(count_true) + ' / Count tests:' + str(count_test * x[k]))
+#     print('Quality = ' + str((count_true / (count_test * x[k])) * 100) + '%')
+#     print("--------------------------------------------------------------------------\n")
+#     y[k] = (count_true / (count_test * x[k])) * 100
+#
+# graphic = plt.plot(x, y)
+# plt.grid(True)
+# plt.scatter(x, y)
+# plt.xlabel('Количество фотографий. Шаг - ' + str(t) + ' фото')
+# plt.ylabel('Точность распознавания')
+# plt.show(graphic)
+
+
+
