e069bef99fc0f10430876f9cb9fc9de316d38941
Polina <chukhninap@gmail.com>
2019-10-13 12:17:03 +0300

    добавлен скрипт для подготовки фото. изменен скрипт для проверки точности

--- testrecognition.py
+++ testrecognition.py
@@ -24,9 +24,9 @@ def image_to_array(filename):
 def face_matrix(count, filename):
 
     face_matr = np.zeros((STANDART_SHAPE, count))
-    for i in range(count):
+    for i in range(0, count):
         face = image_to_array(filename + str(i) + ".png")
-        for j in range(STANDART_SHAPE):
+        for j in range(0, STANDART_SHAPE):
             face_matr[j][i] = face[0][j]
 
     return face_matr
@@ -37,12 +37,12 @@ def face_matrix(count, filename):
 def average_face(av_face, face_matr):
 
     count_of_face = face_matr.shape[1]
-    for i in range(STANDART_SHAPE):
+    for i in range(0, STANDART_SHAPE):
         for j in range(count_of_face):
             av_face[i] += face_matr[i][j]
-            av_face[i] /= count_of_face
+        av_face[i] /= count_of_face
 
-    for i in range(STANDART_SHAPE):
+    for i in range(0, STANDART_SHAPE):
         for j in range(count_of_face):
             face_matr[i][j] -= av_face[i]
 
@@ -50,11 +50,12 @@ def average_face(av_face, face_matr):
 
 
 # calculate pca
+@jit
 def pca(face_matr):
 
     temp_transp = np.zeros((face_matr.shape[1], STANDART_SHAPE))
     for i in range(face_matr.shape[1]):
-        for j in range(STANDART_SHAPE):
+        for j in range(0, STANDART_SHAPE):
             temp_transp[i][j] = face_matr[j][i]
 
     cov_matr = np.matmul(temp_transp, face_matr)
@@ -63,9 +64,9 @@ def pca(face_matr):
     shape = eigvecs.shape[0]
     new_eigvecs = np.zeros((shape, STANDART_SHAPE))
 
-    for i in range(shape):
-        for j in range(shape):
-            for k in range(STANDART_SHAPE):
+    for i in range(0, shape):
+        for j in range(0, shape):
+            for k in range(0, STANDART_SHAPE):
                 new_eigvecs[i][k] += eigvecs[i][j] * face_matr[k][j]
 
     # projected faces from training set into face space
@@ -75,28 +76,28 @@ def pca(face_matr):
 
 # projected faces into face space - вектор лиц уже нормирован по среднему лицу
 # сравнивать два параметра, чтобы точнее идентифицировать лицо - как сравнивать второй параметр?
-@jit()
+@jit
 def transform_new_face(face, eigenvecs, weight_matr):
 
     count = eigenvecs.shape[0]
     weight_vec = np.zeros(count)
-    for i in range(count):
+    for i in range(0, count):
         temp = eigenvecs[i, :]
         weight_vec[i] = np.matmul(temp, face)
 
     # distance from face to face space
     eps_projected = np.zeros(count)
-    for i in range(count):
+    for i in range(0, count):
         eps_projected[i] = np.linalg.norm(weight_vec - weight_matr[:, i])
 
     faces_tilda = np.zeros(STANDART_SHAPE)
-    for i in range(count):
+    for i in range(0, count):
         for j in range(STANDART_SHAPE):
             faces_tilda[j] += weight_vec[i] * eigenvecs[i][j]
     faces_tilda.reshape(STANDART_SHAPE)
 
     temp = np.zeros(STANDART_SHAPE)
-    for i in range(STANDART_SHAPE):
+    for i in range(0, STANDART_SHAPE):
         temp[i] = face[i] - faces_tilda[i]
     eps_face = np.linalg.norm(temp)
 
@@ -130,8 +131,8 @@ class CalculatePCA(object):
         self.weight_matr = temp[1]
         # write matrix pca in file
         file = open('matr_pca.txt', 'w')
-        for i in range(self.eigvecs.shape[0]):
-            for j in range(self.eigvecs.shape[1]):
+        for i in range(0, self.eigvecs.shape[0]):
+            for j in range(0, self.eigvecs.shape[1]):
                 file.write(str(self.eigvecs[i][j]) + '\t')
             file.write('\r\n')
         file.close()
@@ -144,23 +145,23 @@ class CalculatePCA(object):
         temp_result = average_face(self.av_face, self.face_matr_train)
         self.av_face = temp_result[1]
         self.face_matr_train = temp_result[0]
-        temp_pca = np.genfromtxt('matr_pca.txt', dtype=np.float, delimiter='\t')
-        shape_pca = temp_pca.shape
-        new_pca = np.zeros((shape_pca[0], shape_pca[1] - 1))
-        for i in range(shape_pca[0]):
-            # тут -1, потому что записываем в файл пустую строку после последней строки матрицы с собственными векторами
-            for j in range(shape_pca[1] - 1):
-                self.eigvecs[i][j] = temp_pca[i][j]
-        #         new_pca[i][j] = temp_pca[i][j]
-        # self.eigvecs = new_pca
-        self.weight_matr = np.matmul(self.eigvecs, self.face_matr_train)
+        # temp_pca = np.genfromtxt('matr_pca.txt', dtype=np.float, delimiter='\t')
+        # shape_pca = temp_pca.shape
+        # new_pca = np.zeros((shape_pca[0], shape_pca[1] - 1))
+        # for i in range(0, shape_pca[0]):
+        #     # тут -1, потому что записываем в файл пустую строку после последней строки матрицы с собственными векторами
+        #     for j in range(0, shape_pca[1] - 1):
+        #         self.eigvecs[i][j] = temp_pca[i][j]
+        # #         new_pca[i][j] = temp_pca[i][j]
+        # # self.eigvecs = new_pca
+        # self.weight_matr = np.matmul(self.eigvecs, self.face_matr_train)
 
         return
 
     def transform_faces(self, test_filename):
         self.face_matr_test = face_matrix(self.count_test_set, test_filename)
         # normalizing test image
-        for i in range(STANDART_SHAPE):
+        for i in range(0, STANDART_SHAPE):
             self.face_matr_test[i] -= self.av_face[i]
         result = transform_new_face(self.face_matr_test, self.eigvecs, self.weight_matr)
 
